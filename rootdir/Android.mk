1c046eba2b00c65b5d6566fa4b5860302682bf52
Flagers <flagersmail@gmail.com>
2019-05-22 16:25:13 -0400

    Revert "Revert "Revert "qcom318-32: move init.*.rc to vendor"""
    This reverts commit 124b07e3e79d0c0c3a4af0cb069e54705bdc5173.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE       := init.mmi.boot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mmi.boot.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -15,7 +15,7 @@ LOCAL_MODULE       := init.mmi.laser.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mmi.laser.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -23,7 +23,7 @@ LOCAL_MODULE       := init.mmi.touch.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mmi.touch.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -31,7 +31,7 @@ LOCAL_MODULE       := wlan_carrier_bin.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/wlan_carrier_bin.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

ff81181e16a31f7c5b4b99cc90fcccf21d167752
Flagers <flagersmail@gmail.com>
2019-05-24 00:04:55 -0400

    rootdir: fix a bunch of init script location issues in kernel log and remove mmi.touch
    *fixes:
    "[    1.935375,1] init: Unable to open '/system/vendor/etc/init/hw/init.mmi.rc': No such file or directory
    [    1.935398,1] init: /init.rc: 28: Could not import file '/system/vendor/etc/init/hw/init.mmi.rc': No such file or directory
    [    1.935421,1] init: Parsing file /system/vendor/etc/init/hw/init.platform.rc...
    [    1.935449,1] init: Unable to open '/system/vendor/etc/init/hw/init.platform.rc': No such file or directory
    [    1.935471,1] init: /init.rc: 29: Could not import file '/system/vendor/etc/init/hw/init.platform.rc': No such file or directory
    [    1.935493,1] init: Parsing file /system/vendor/etc/init/hw/init.target.rc...
    [    1.935520,1] init: Unable to open '/system/vendor/etc/init/hw/init.target.rc': No such file or directory
    [    1.935542,1] init: /init.rc: 30: Could not import file '/system/vendor/etc/init/hw/init.target.rc': No such file or directory
    [    1.935578,1] init: Parsing file /vendor/etc/init/hw/init.qcom.rc...
    [    1.935610,1] init: Unable to open '/vendor/etc/init/hw/init.qcom.rc': No such file or directory
    [    1.935632,1] init: /init.rc: 10: Could not import file '/vendor/etc/init/hw/init.qcom.rc': No such file or directory"
    *remove mmi.touch because it is broken

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE       := init.mmi.boot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mmi.boot.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -15,15 +15,7 @@ LOCAL_MODULE       := init.mmi.laser.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mmi.laser.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mmi.touch.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mmi.touch.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -31,14 +23,7 @@ LOCAL_MODULE       := wlan_carrier_bin.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/wlan_carrier_bin.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.bt.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -46,6 +31,15 @@ LOCAL_MODULE       := init.qcom.ril.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.ril.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.bt.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 # Init scripts
@@ -55,7 +49,7 @@ LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -63,7 +57,7 @@ LOCAL_MODULE       := init.mmi.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mmi.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -71,7 +65,7 @@ LOCAL_MODULE       := init.mmi.usb.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mmi.usb.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -79,5 +73,5 @@ LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
